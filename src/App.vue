d137950018f0d13fd1c3ec22a11c09152e81f7b1
RossanaFrouf <rossana.frouf@gmail.com>
2024-12-02 21:34:40 -0300

    aplicação da lógica e estilo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script setup>
+import { ref } from "vue";
 import coracaoCheio from "../src/assets/coracaoCheio.png"
 import coracaoVazio from "../src/assets/coracaoVazio.png"
+
+
+const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString('pt-BR', options);
+};
+
+const imagens = ref([
+  { id: 1, src: coracaoVazio, cheio: false, data: '2024-12-02'},
+  { id: 2, src: coracaoVazio, cheio: false, data: '2024-11-31'},
+  { id: 3, src: coracaoVazio, cheio: false, data: '2024-08-10'},
+  { id: 4, src: coracaoVazio, cheio: false, data: '2024-08-10'},
+
+]);
+
+const alterarImagem = (id) => {
+  const imagem = imagens.value.find((img) => img.id === id);
+  if (imagem) {
+    imagem.cheio = !imagem.cheio;
+    imagem.src = imagem.cheio ? coracaoCheio : coracaoVazio;
+  }
+};
 
 </script>
 
@@ -10,66 +33,19 @@ import coracaoVazio from "../src/assets/coracaoVazio.png"
   <input type="text" placeholder="Pesquisar no blog">
  </header>
  <main>
-  <section>
+  <section v-for="imagem in imagens" :key="imagem.id">
     <div>
-      <p>17 de ago, 2024</p>
+      <p>{{ formatDate(imagem.data) }}</p>      
         <!-- Como trazer dados de forma dinamica ?-->
         <!-- dentro de Tags, usamos o {} ex: <p {} </p>  -->
         <!-- para inserir em atributos, usamos o v-bind
         : atalho para o v-bind -->
-      <img v-bind:src="coracaoCheio" alt="coração cheio">
-    </div>
-    <h3>O que é linguagem de programação? Conheça as principais</h3>
-    <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
+        <img :src="imagem.src" :alt="'Coração ' + (imagem.cheio ? 'cheio' : 'vazio')" @click="alterarImagem(imagem.id)"/>
+    <h3>O que é linguagem de programação? Conheça as principais:</h3>
+    <h4>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</h4>
+  </div>
   </section>
-  <section>
-    <div>
-      <p>17 de ago, 2024</p>
-        <!-- Como trazer dados de forma dinamica ?-->
-        <!-- dentro de Tags, usamos o {} ex: <p {} </p>  -->
-        <!-- para inserir em atributos, usamos o v-bind
-        : atalho para o v-bind -->
-      <img v-bind:src="coracaoCheio" alt="coração cheio">
-    </div>
-    <h3>O que é linguagem de programação? Conheça as principais</h3>
-    <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
-  </section>
-  <section>
-    <div>
-      <p>17 de ago, 2024</p>
-        <!-- Como trazer dados de forma dinamica ?-->
-        <!-- dentro de Tags, usamos o {} ex: <p {} </p>  -->
-        <!-- para inserir em atributos, usamos o v-bind
-        : atalho para o v-bind -->
-      <img v-bind:src="coracaoCheio" alt="coração cheio">
-    </div>
-    <h3>O que é linguagem de programação? Conheça as principais</h3>
-    <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
-  </section>
-  <section>
-    <div>
-      <p>17 de ago, 2024</p>
-        <!-- Como trazer dados de forma dinamica ?-->
-        <!-- dentro de Tags, usamos o {} ex: <p {} </p>  -->
-        <!-- para inserir em atributos, usamos o v-bind
-        : atalho para o v-bind -->
-      <img v-bind:src="coracaoVazio" alt="coração vazio">
-    </div>
-    <h3>O que é linguagem de programação? Conheça as principais</h3>
-    <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
-  </section>
-  <section>
-    <div>
-      <p>17 de ago, 2024</p>
-        <!-- Como trazer dados de forma dinamica ?-->
-        <!-- dentro de Tags, usamos o {} ex: <p {} </p>  -->
-        <!-- para inserir em atributos, usamos o v-bind
-        : atalho para o v-bind -->
-      <img v-bind:src="coracaoVazio" alt="coração vazio">
-    </div>
-    <h3>O que é linguagem de programação? Conheça as principais</h3>
-    <p>Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.</p>
-  </section>
+  
  </main>
 </template>
 
@@ -91,12 +67,58 @@ header{
     margin-top: 1rem;
     background-color: #202024;
     border: none;
-    padding: 8px 0;
+    padding: 8px 8px;
+    color: #FFFFFF;l
 
     &::placeholder{
         padding-left: 10px;
-        color: red;
+        color: #AFABB6;      
     }
   }
+}
+
+
+main {
+  display: flex;
+  flex-direction: column;
+  padding: 0 20%;
+  justify-content: center;
+  background-color: black;
+  font-family: "Space Grotesk", sans-serif;
+}
+
+div {
+  width: 60vw;
+  background-color: #202024;
+  border: none;
+  padding: 40px 40px;
+  margin: 40px 20px;
+
+  &:hover{
+    border: #E07B67 solid;
+  
+  }
+
+}
+
+img{
+ margin: 0 90%;
+ padding: 0% 8%;
+}
+
+p{
+  color: #E07B67;
+
+}
+
+h3 {
+  color: #FFFFFF;
+  padding: 5px;
+}
+
+
+h4{
+  padding: 5px;
+  color: #AFABB6;
 }
 </style>
